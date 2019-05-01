692bd94aafec658cbb574614864ff571c1c7f653
Anthony <anthonymarques@outlook.com>
2019-05-01 16:15:01 +0200

    added service and working on event object/array

--- src/app/createevent/createevent.component.ts
+++ src/app/createevent/createevent.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'createevent',
   templateUrl: './createevent.component.html',
   styleUrls: ['./createevent.component.css']
 })
-export class CreateeventComponent implements OnInit {
-
+export class CreateeventComponent  {
+  events = [
+    {
+      name: 'This Sunday',
+      date: '05-04-2019',
+      location: 'Mowbray',
+      time: '09:00 for an hour',
+      confirmed: [
+        {name: 'Tony'},
+        {name: 'Doggy'},
+        {name: 'Kyle'}
+      ],
+      maybe: [
+        {name: 'Lyle'}
+      ]
+    }
+  ]
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
--- src/app/createevent/events.service.ts
+++ src/app/createevent/events.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getEvents() {
+    return this.http.get('https://teamfutbeta.firebaseio.com/')
+  }
+
+  updateEvents(event:any[]) {
+    return this.http.put('https://teamfutbeta.firebaseio.com/', event)
+  }
 }
