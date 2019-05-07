ddb6d6a3264dd104ee7b2e6e5df73f88a2f7a956
Anthony <anthony.marque@gmail.com>
2019-05-07 14:00:51 +0200

    working on dynamic form fields

--- src/app/createevent/createevent.component.ts
+++ src/app/createevent/createevent.component.ts
@@ -1,25 +1,40 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { EventsService } from "../services/events.service";
 import { Response } from "@angular/http";
-import { Observable } from 'rxjs';
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "createevent",
   templateUrl: "./createevent.component.html",
   styleUrls: ["./createevent.component.css"]
 })
-export class CreateeventComponent {
-  events:any[];
-    // {
-    //   title: "This Sunday",
-    //   date: "05-04-2019",
-    //   location: "Mowbray",
-    //   time: "09:00 for an hour",
-    //   confirmed: [{ name: "Tony" }, { name: "Doggy" }, { name: "Kyle" }],
-    //   maybe: [{ name: "Lyle" }]
-    // }
-  
-  constructor(private eventsService: EventsService) {}
+export class CreateeventComponent implements OnInit {
+  events: any[];
+  public form: FormGroup;
+  public confirmedList: FormArray;
+  public maybeList: FormArray;
+
+  constructor(private fb: FormBuilder, private eventsService: EventsService) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: [null, Validators.compose([Validators.required])],
+      confirmList: this.fb.array([this.confirmedList]),
+      maybeList: this.fb.array([this.maybeList])
+    });
+
+    this.confirmedList = this.form.get('confirmed') as FormArray;
+    this.maybeList = this.form.get('maybe') as FormArray;
+  }
+
+  createEvent(): FormGroup {
+    return this.fb.group({
+      title: [null, Validators.compose([Validators.required])],
+      date: [null, Validators.compose([Validators.required])],
+      time: [null, Validators.compose([Validators.required])],
+      location: [null, Validators.compose([Validators.required])]
+    }); 
+  }
 
   addEvent() {
     this.eventsService
@@ -29,6 +44,4 @@ export class CreateeventComponent {
         error => console.log(error)
       );
   }
-
- 
 }
