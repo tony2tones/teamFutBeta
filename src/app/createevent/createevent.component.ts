71fdc99573929aee98ccd75416cce41578c70ac2
Anthony <anthony.marque@gmail.com>
2019-05-07 16:55:52 +0200

    commented out formbuilder goodies to have a working build for now

--- src/app/createevent/createevent.component.ts
+++ src/app/createevent/createevent.component.ts
@@ -8,33 +8,33 @@ import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
   templateUrl: "./createevent.component.html",
   styleUrls: ["./createevent.component.css"]
 })
-export class CreateeventComponent implements OnInit {
+export class CreateeventComponent {
   events: any[];
   public form: FormGroup;
   public confirmedList: FormArray;
   public maybeList: FormArray;
 
-  constructor(private fb: FormBuilder, private eventsService: EventsService) {}
+  constructor(private eventsService: EventsService) {}
 
-  ngOnInit() {
-    this.form = this.fb.group({
-      name: [null, Validators.compose([Validators.required])],
-      confirmList: this.fb.array([this.confirmedList]),
-      maybeList: this.fb.array([this.maybeList])
-    });
+  // ngOnInit() {
+  //   this.form = this.fb.group({
+  //     name: [null, Validators.compose([Validators.required])],
+  //     confirmList: this.fb.array([this.confirmedList]),
+  //     maybeList: this.fb.array([this.maybeList])
+  //   });
 
-    this.confirmedList = this.form.get('confirmed') as FormArray;
-    this.maybeList = this.form.get('maybe') as FormArray;
-  }
+  //   this.confirmedList = this.form.get('confirmed') as FormArray;
+  //   this.maybeList = this.form.get('maybe') as FormArray;
+  // }
 
-  createEvent(): FormGroup {
-    return this.fb.group({
-      title: [null, Validators.compose([Validators.required])],
-      date: [null, Validators.compose([Validators.required])],
-      time: [null, Validators.compose([Validators.required])],
-      location: [null, Validators.compose([Validators.required])]
-    }); 
-  }
+  // createEvent(): FormGroup {
+  //   return this.fb.group({
+  //     title: [null, Validators.compose([Validators.required])],
+  //     date: [null, Validators.compose([Validators.required])],
+  //     time: [null, Validators.compose([Validators.required])],
+  //     location: [null, Validators.compose([Validators.required])]
+  //   }); 
+  // }
 
   addEvent() {
     this.eventsService
