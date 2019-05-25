2c01163a823ba4d631ee61739b13d78ee38ae848
Anthony <anthonymarques@outlook.com>
2019-05-25 18:18:04 +0200

    working on forms

--- src/app/createevent/createevent.component.ts
+++ src/app/createevent/createevent.component.ts
@@ -1,45 +1,47 @@
 import { Component, OnInit } from "@angular/core";
 import { EventsService } from "../services/events.service";
 import { Response } from "@angular/http";
-import { FormArray, FormBuilder, FormGroup } from "@angular/forms";
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormControl
+} from "@angular/forms";
 
+import { Event } from "../model/event.model";
 @Component({
   selector: "createevent",
   templateUrl: "./createevent.component.html",
   styleUrls: ["./createevent.component.css"]
 })
-export class CreateeventComponent implements OnInit{
-  events: any[];
-  myForm: FormGroup;
-  // public confirmedList: FormArray;
-  // public maybeList: FormArray;
+export class CreateeventComponent implements OnInit {
+  event: Event;
+  eventForm: FormGroup;
+  attendingState: Array<string> = ["Confirmed", "maybe"];
+  players:any[];
+  // player = { name: "", status: "" };
 
-  constructor(private fb: FormBuilder,private eventsService: EventsService) {}
-  
+  constructor(private fb: FormBuilder, private eventsService: EventsService) {}
+
   ngOnInit() {
-    this.myForm = this.fb.group({
-      title: '',
-      confirmList: this.fb.array([])
-      // maybeList: this.fb.array([])
+    this.eventForm = this.fb.group({
+      // title: ['', Validators.compose([Validators.required])],
+      name: ["", Validators.compose([Validators.required])],
+      state: ["", Validators.compose([Validators.required])]
     });
   }
 
-    get confimredForm() {
-      return this.myForm.get('confirmList') as FormArray;
-    }
+  addConfirmed() {
+    let player = this.eventForm.value;
+    this.players.push(this.players, {...player });
+    console.log("players collection value ", this.players);
+    // this.player = { name: "", status: "" };
+  }
 
-    addConfirmed() {
-      const confirmed = this.fb.group({
-        name:[]
-      });
-      this.confimredForm.push(confirmed);
-      this.events.push(confirmed);
-      console.log('loggin this ',this.events)
-    }
 
-    deleteConfirmed(i) {
-      this.confimredForm.removeAt(i);
-    }
+  // deleteConfirmed(i) {
+  //   this.confimredForm.removeAt(i);
+  // }
   //   this.confirmedList = this.form.get('confirmed') as FormArray;
   //   this.maybeList = this.form.get('maybe') as FormArray;
   // }
@@ -50,15 +52,22 @@ export class CreateeventComponent implements OnInit{
   //     date: [null, Validators.compose([Validators.required])],
   //     time: [null, Validators.compose([Validators.required])],
   //     location: [null, Validators.compose([Validators.required])]
-  //   }); 
+  //   });
   // }
 
   addEvent() {
-    this.eventsService
-      .updateEvents(this.events)
-      .subscribe(
-        (response: Response) => console.log(response),
-        error => console.log(error)
-      );
+    this.players = this.eventForm.value;
+    console.log(
+      "event for value ",
+      this.eventForm.value,
+      "players collection value ",
+      this.players
+    );
+    // this.eventsService
+    //   .updateEvents(this.events)
+    //   .subscribe(
+    //     (response: Response) => console.log(response),
+    //     error => console.log(error)
+    //   );
   }
 }
