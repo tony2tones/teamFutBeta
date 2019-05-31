21062dc54dc107968a8c0513bf8a23d5731846a9
Anthony <anthony.marque@gmail.com>
2019-05-31 14:03:53 +0200

    removed console loggin

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -1,44 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { EventsService } from '../services/events.service';
+import { Component, OnInit } from "@angular/core";
+import { EventsService } from "../services/events.service";
 
 @Component({
-  selector: 'app-eventlist',
-  templateUrl: './eventlist.component.html',
-  styleUrls: ['./eventlist.component.css']
+  selector: "app-eventlist",
+  templateUrl: "./eventlist.component.html",
+  styleUrls: ["./eventlist.component.css"]
 })
 export class EventlistComponent implements OnInit {
-
-  constructor(private eventService : EventsService) { }
-  events:any[];
-  public players;
+  constructor(private eventService: EventsService) {}
+  events: any[];
+  players: any[];
   ngOnInit() {
     this.getEvents();
-    
   }
 
   getEvents() {
-    this.eventService
-      .getEvents()
-      .subscribe(
-        (event:any) => {
-          this.events = event;
-          console.log('getting events if there is any ', event);
-          console.log('getting events if there is any ', event[1].name, ' ', event[1].state);
-          console.log('getting events if there is any ', event[2].name, ' status ', event[2].state);
-          this.setPlayerState(this.events);
-        },
-        error => console.log(error)
-      );
+    this.eventService.getEvents().subscribe(
+      (event: any) => {
+        this.events = event;
+        this.setPlayerState(this.events);
+      },
+      error => console.log(error)
+    );
   }
 
-  setPlayerState(data:any[]) {
+  setPlayerState(data: any[]) {
     let playerState = [];
     var arrayLength = data.length;
-    for(var i = 1; i < arrayLength; i++){
+    for (var i = 1; i < arrayLength; i++) {
       playerState.push(data[i]);
-      console.log('player state ', playerState);
       this.players = playerState;
     }
   }
-
 }
