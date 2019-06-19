f9d7e7a3765c36f3801c3af0343194659c9d0f25
Anthony <anthonymarques@outlook.com>
2019-06-19 18:32:30 +0200

    added auth service Adding spinner

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,27 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import  {NgForm}  from '@angular/forms'
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"]
 })
 export class AuthComponent implements OnInit {
+  loginMode: boolean = true;
 
-  loginMode:boolean = true;
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   switchMode() {
     this.loginMode = !this.loginMode;
-    
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
-  }
+    const email = form.value.email;
+    const password = form.value.password;
 
+    if (this.loginMode) {
+      
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        respData => {
+          console.log(respData);
+        },
+        error => console.log(error)
+      );
+      
+    }
+    form.reset();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,15 +10,16 @@ interface AuthResponseData {
   localId: string;
 }
 
+
 @Injectable({ providedIn: "root" })
 export class AuthService {
-  apiKey = "AIzaSyAzG5Vv0eRvIaeimyQamz8u3qj8oKwyG7o";
+    apiKey = "AIzaSyAzG5Vv0eRvIaeimyQamz8u3qj8oKwyG7o";
 
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
     return this.http.post<AuthResponseData>(
-      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${apiKey}`,
+      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${this.apiKey}`,
       {
         email: email,
         password: password,
