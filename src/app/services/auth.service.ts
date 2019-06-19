1fac1d0179588d880f6fff8e4322e5fdf850d6c9
Anthony <anthonymarques@outlook.com>
2019-06-19 16:33:59 +0200

    add interface for AuthResponsedata

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
-@Injectable({providedIn: 'root'})
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
 
+@Injectable({ providedIn: "root" })
 export class AuthService {
-    apiKey="AIzaSyAzG5Vv0eRvIaeimyQamz8u3qj8oKwyG7o";
-    
-    constructor (private http: HttpClient) {}
+  apiKey = "AIzaSyAzG5Vv0eRvIaeimyQamz8u3qj8oKwyG7o";
 
-    signUp(email: string, password:string ) {
-        return this.http.post(`https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${apiKey}`,
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true
-        })
-    }
+  constructor(private http: HttpClient) {}
+
+  signUp(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=${apiKey}`,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
+  }
 }
