30c84e7e70ad64d0e4a0acccd0b96d32a40741a1
Anthony <anthonymarques@outlook.com>
2019-06-24 19:56:22 +0200

    added auth checker

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
-
+export class MainNavComponent implements OnInit, OnDestroy{
+  private userSub: Subscription;
+  isAuthenticated: boolean = false;
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private authService: AuthService) {}
+
+  ngOnInit() {
+   this.userSub = this.authService.user.subscribe(user => {
+    this.isAuthenticated = !!user;
+   });
+  }
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 
 }
