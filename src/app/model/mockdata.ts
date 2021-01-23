4339c8e21647bde2798570cef55e03bc77037ee7
tony2tones <anthonymarques@outlook.com>
2021-01-23 16:47:20 +0200

    keep working on dashboard

--- src/app/model/mockdata.ts
+++ src/app/model/mockdata.ts
@@ -45,6 +45,35 @@ export const games = [
       { name: 'Kenny', state: 'maybe' },
       { name: 'Serg', state: 'confirmed' }
     ]
+  },
+  {
+    id: '3',
+    title: 'Fives Sunday',
+    location: 'Rhodes',
+    date: '18 Jan 2021',
+    time: '09:00',
+    players: [
+      { name: 'Tony', state: 'confirmed' },
+      { name: 'Donny', state: 'confirmed' },
+      { name: 'Kenny', state: 'maybe' },
+      { name: 'Serg', state: 'confirmed' },
+      { name: 'Farrel', state: 'confirmed' },
+      { name: 'Kyle', state: 'confirmed' },
+      { name: 'Braulio', state: 'confirmed' }
+    ]
+  },
+  {
+    id: '4',
+    title: 'Mid week madness Wednesday',
+    location: 'Century City',
+    date: '24 Jan 2021',
+    time: '11:00',
+    players: [
+      { name: 'Tony', state: 'confirmed' },
+      { name: 'Donny', state: 'confirmed' },
+      { name: 'Kenny', state: 'maybe' },
+      { name: 'Serg', state: 'confirmed' }
+    ]
   }
 ]
 
