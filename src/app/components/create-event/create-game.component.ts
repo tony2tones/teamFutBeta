c591edf77236c3be529f8595e9e56f076187f19f
tony2tones <anthonymarques@outlook.com>
2021-08-19 17:57:06 +0200

    fixed loader with toasty notification that the game has been created

--- src/app/components/create-event/create-game.component.ts
+++ src/app/components/create-event/create-game.component.ts
@@ -1,20 +1,20 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { GameService } from '../../services/games.service';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ToastMessageService } from '../../services/toast-message.service';
+import { Component, ViewChild, OnInit } from "@angular/core";
+import { GameService } from "../../services/games.service";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ToastMessageService } from "../../services/toast-message.service";
 
-import { MatStepper } from '@angular/material';
-import { Game } from '../../model/game.model';
-import { Player } from '../../model/game.model';
+import { MatStepper } from "@angular/material";
+import { Game } from "../../model/game.model";
+import { Player } from "../../model/game.model";
 
 export interface User {
   name: string;
 }
 @Component({
-  selector: 'create-game',
-  templateUrl: './create-game.component.html',
-  styleUrls: ['./create-game.component.css']
+  selector: "create-game",
+  templateUrl: "./create-game.component.html",
+  styleUrls: ["./create-game.component.css"],
 })
 export class CreateGameComponent implements OnInit {
   eventForm: FormGroup;
@@ -22,13 +22,14 @@ export class CreateGameComponent implements OnInit {
 
   event: Game;
   isLinear = true;
-  @ViewChild('step1', {static: false}) stepper: MatStepper;
+  @ViewChild("step1", { static: false }) stepper: MatStepper;
 
-  attendingState: Array<string> = ['Confirmed', 'Maybe'];
+  attendingState: Array<string> = ["Confirmed", "Maybe"];
   gameDetails: Game[] = [];
   details: any;
   players: Player[] = [];
   player: Player;
+  isLoading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -39,15 +40,15 @@ export class CreateGameComponent implements OnInit {
 
   ngOnInit() {
     this.detailsForm = this.fb.group({
-      title: ['Sunday Game', Validators.compose([Validators.required])],
-      location: ['Mowbray', Validators.compose([Validators.required])],
-      date: ['this sunday', Validators.compose([Validators.required])],
-      time: ['09:00', Validators.compose([Validators.required])],
-      players : []
+      title: ["Sunday Game", Validators.compose([Validators.required])],
+      location: ["Mowbray", Validators.compose([Validators.required])],
+      date: ["this sunday", Validators.compose([Validators.required])],
+      time: ["09:00", Validators.compose([Validators.required])],
+      players: [],
     });
     this.eventForm = this.fb.group({
-      name: [''],
-      state: ['', Validators.compose([Validators.required])]
+      name: [""],
+      state: ["", Validators.compose([Validators.required])],
     });
   }
 
@@ -63,54 +64,52 @@ export class CreateGameComponent implements OnInit {
 
   addGameDetails() {
     // let group;
-    console.log('details ', this.details, 'players ', ...this.players);
+    console.log("details ", this.details, "players ", ...this.players);
     this.gameDetails.push(this.details);
     this.details.players = this.players;
-    console.log('THE DEETs', this.gameDetails);
+    console.log("THE DEETs", this.gameDetails);
     // this.gameDetails = group.concat(this.details, this.players);
     // this.gameDetails = group = this.details.push(this.players)
   }
 
   deleteConfirmed(name: string) {
-    return this.players.filter(value => {
-      if(value.name === name) {
-        return this.players.splice(this.players.indexOf(value),1);
+    return this.players.filter((value) => {
+      if (value.name === name) {
+        return this.players.splice(this.players.indexOf(value), 1);
       }
-    }) 
+    });
   }
 
   // https://www.reddit.com/r/learnprogramming/comments/bs4vjf/tsfirebaseerror_function_documentreferenceset/
 
   CraeteGameEvent() {
     let record = {};
-    record['date']=this.details.date;
-    record['location'] = this.details.location;
-    record['players'] = this.details.players;
-    record['time'] = this.details.time;
-    record['title'] = this.details.title;
-
+    record["date"] = this.details.date;
+    record["location"] = this.details.location;
+    record["players"] = this.details.players;
+    record["time"] = this.details.time;
+    record["title"] = this.details.title;
+    this.isLoading = true;
     // record['date'] = this.gameDetails.date;
-    console.log('submitted details ', this.gameDetails)
-    this.gameService.createGame(record).then(() => {
-      console.log('error');
-    })
-    // this.GameService.updateEvents(this.gameDetails).subscribe(
-    //   repsonse => {
-    //   console.log('the full details ', this.gameDetails);
-
-    //     this.router.navigate(['/']),
-    //       this.toastr.showSuccess(
-    //         'Has been successfully created.',
-    //         'Footy game'
-    //       );
-    //   },
-    //   error => {
-    //     console.log(error);
-    //     this.toastr.showFail(
-    //       'Something went wrong, please select the reset option to try again.',
-    //       'Error'
-    //     );
-    //   }
-    // );
+    console.log("submitted details ", this.gameDetails);
+    this.gameService.createGame(record).then(
+      (response) => {
+        console.log("the full details ", this.gameDetails);
+        console.log("the full response ", response);
+        this.isLoading = false;
+        this.router.navigate(["/"]),
+          this.toastr.showSuccess(
+            "Has been successfully created.",
+            "Footy game"
+          );
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.showFail(
+          "Something went wrong, please select the reset option to try again.",
+          "Error"
+        );
+      }
+    );
   }
 }
